4fa4c1fbcfd96e2c6b55106b2e84bad14a82bac8
Adei Josol <adjl@users.noreply.github.com>
2014-04-29 05:26:52 +0100

    Refine notes more

--- Cam.cpp
+++ Cam.cpp
@@ -1,21 +1,4 @@
-void GLWidget::mouseMoveEvent(QMouseEvent *event)
-{
-  if (event->buttons() & Qt::LeftButton) {
-    switch (activeViewport) { // Move cameras in viewports accordingly
-      case ISO:   isoCamera->setDeltaTheta(toRadians(xClick - event->x() + (width / 2)));
-                  isoCamera->setDeltaPhi(toRadians(event->y() - yClick));
-                  break;
-    }
-  }
-}
-
-void GLWidget::mouseReleaseEvent(QMouseEvent *event)
-{
-  if (event->button() == Qt::LeftButton) {
-    switch (activeViewport) {
-      case ISO:   isoCamera->updateTheta(isoCamera->getDeltaTheta());
-                  isoCamera->updatePhi(isoCamera->getDeltaPhi());
-                  break;
-    }
-  }
-}
+isoCamera->setDeltaTheta(toRadians(xClick - event->x());
+isoCamera->setDeltaPhi(toRadians(event->y() - yClick));
+isoCamera->updateTheta(dTheta);
+isoCamera->updatePhi(dPhi);
